57f25730864105237b8128b04625ed6f9dc085ee
javiermartins <javiimartinserrano@gmail.com>
2024-09-02 20:13:20 +0200

    Custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,21 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
+            colors: {
+                'primary': '#415962',
+                'secondary': {
+                    100: "#e6f2e6",
+                    200: "#cce6cc",
+                    300: "#b3d9b3",
+                    400: "#99cc99",
+                    500: "#81c683",
+                    600: "#73b973",
+                    700: "#66ac66",
+                    800: "#589e58",
+                    900: "#4b914b"
+                },
+                'accent': '#ffa51e'
+            },
             keyframes: {
                 slidein: {
                     from: {
